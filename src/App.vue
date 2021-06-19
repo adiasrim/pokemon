3dc30af3c425ab71ae92355ec85d2d14b01b7701
MirikAkhmedov <akhmedovmirik@gmail.com>
2021-06-19 06:35:49 +0500

    connect axios;

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="p-14">
-    <router-link class="flex justify-center text-4xl text-yellow-700" to="/">Pokemon Picker</router-link>
-  </div>
-  <router-view/>
+    <div class="p-14">
+        <router-link class="flex justify-center text-4xl text-yellow-700" to="/">Pokemon Picker</router-link>
+    </div>
+    <router-view/>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,61 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-  </div>
+    <div class="w-full flex justify-center">
+        <input
+            type="text"
+            placeholder="Enter pokemon here: "
+            class="mt-10 p-2 border-blue-500 border-2"
+            v-model="text"
+        />
+    </div>
+    <div class="mt-10 p-4 flex flex-wrap justify-center">
+        <div
+            class="ml-4 text-2x text-blue-500"
+            v-for="(pokemon, index) of filteredPokemons"
+            :key="index"
+        >
+            {{pokemon.name}}
+        </div>
+        <ul v-for="pokemon in pokemons">
+            <li style="border: 1px solid black; margin: 2px">{{pokemon.name}}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
-
+import {reactive, toRefs, computed} from "vue";
+import axios from "axios";
 export default {
+    name: 'Home',
+    setup() {
+        const state = reactive({
+            pokemons: [],
+            ulrIdLookUp: {},
+            text: '',
+            filteredPokemons: computed( () => updatePokemon() )
+        })
+        const updatePokemon = () => {
+            if (!state.text){
+                return console.log('hello');
+            }
+
+            return state.pokemons.filter( pokemon => {
+                pokemon.name.includes(state.text);
+            })
+        }
+        axios.get('https://pokeapi.co/api/v2/pokemon?offset=0')
+            .then( res => res.data )
+            .then( data => {
+                state.pokemons = data.results;
+                state.ulrIdLookUp = data.results.reduce((acc, cur, idx) =>
+                    acc = {
+                        ...acc,
+                        [cur.name]:idx + 1
+                    },
+                    {}
+                )
+            })
+        return {...toRefs(state)}
+    }
+
 }
 </script>
